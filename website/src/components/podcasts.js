3e872b974aa7f17156375d8f0188b66cd3c34477
James McLeod <james.mcleod@finos.org>
2022-06-08 12:23:22 +0100

    add current live podcasts

--- website/src/components/podcasts.js
+++ website/src/components/podcasts.js
@@ -2,48 +2,39 @@ import React from 'react';
 
 export const podcasts = [ 
     {
-        title : "The Void: Verica Open Incident Database",
-        src : "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1244739151&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
-        type : "soundcloud",
-        description : "In this episode, we're headed to The VOID. I speak with Courtney Nash about the Verica Open Incident Database.",
-        podcast : "The OWASP Podcast Series",
-        podcastUrl : "https://soundcloud.com/owasp-podcast",
-        featured : true
-    },
-    {
-        title : "Fast Times at SBOM High",
-        src : "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1237986250&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
-        type : "soundcloud",
-        description : "I have the pleasure of reuniting with Wendy Nather, CISO Advisor Extraordinaire, for this episode. We had a very interesting conversation about Software Bill of Materials (SBOMs).",
-        podcast : "The OWASP Podcast Series",
-        podcastUrl : "https://soundcloud.com/owasp-podcast",
-        featured : true
-    },
-    {
-        title : "SAFe or UnSAFe at Any Speed",
-        src : "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1231124572&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
-        type : "soundcloud",
-        description : "I was scrolling through LinkedIn a couple days ago, saw a thread on SAFe, The Scaled Agile Framework, and what I was seeing wasn’t exactly...",
-        podcast : "The OWASP Podcast Series",
-        podcastUrl : "https://soundcloud.com/owasp-podcast",
-        featured : true
-    },
-    {
-        title : "Compliant Financial Infrastructure & Eddie Knight - Managing Consultant, Synechron",
-        src : "https://open.spotify.com/embed/episode/0PVzsVlqk41DZizqJgAoHo?utm_source=generator",
+        title : "Patrick Debois. Business of Open Source",
+        src : "https://open.spotify.com/embed/episode/3MKsXkw9Et5B9bGLWKJNpc?utm_source=generator",
         type : "spotify",
-        description : "In this episode of the of the podcast, James McLeod, FINOS Director of Community sits down with Eddie Knight, Managing Consultant at Synechron.",
-        podcast : "FINOS Open Source in Finance Podcast",
-        podcastUrl : "https://open.spotify.com/show/431XdfX2fBdCUsopwh8td3",
+        description : "The first time Patrick Debois came into contact with Open Source was in the early stages of development of the Linux kernel, compiling it on floppies on his 486 machine. To tell you how long ago that was, the Intel 486 was introduced in 1989, It was the first chip in the line to include a built-in math coprocessor.",
+        podcast : "The Untold Stories of Open Source",
+        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
         featured : true
     },
     {
-        title : "Hello James - Compliant Financial Infrastructure & Eddie Knight - Managing Consultant, Synechron",
-        src : "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1231124572&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true",
-        type : "soundcloud",
-        description : "In this episode of the of the podcast, James McLeod, FINOS Director of Community sits down with Eddie Knight, Managing Consultant at Synechron.",
-        podcast : "FINOS Open Source in Finance Podcast",
-        podcastUrl : "https://open.spotify.com/show/431XdfX2fBdCUsopwh8td3",
+        title : "Brian Behlendorf and the OpenSSF Project",
+        src : "https://open.spotify.com/embed/episode/0P0cjBDn5nSYPe1i0FrFXr?utm_source=generator",
+        type : "spotify",
+        description : "Brian Behlendorf came from a science and technology background. In fact, his parents met at IBM where his father was a Cobol programmer. During the 1980s, Brian was comfortable in front of a TRS 80 and a PC junior doing basic programming and term reports.",
+        podcast : "The Untold Stories of Open Source",
+        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
         featured : true
-    }    
+    },
+    {
+        title : "Priyanka Sharma, GM, CNCF",
+        src : "https://open.spotify.com/embed/episode/5KgqNXHHV0y03yjgg7kg8E?utm_source=generator",
+        type : "spotify",
+        description : "Priyanka Sharma has had a long career in tech. After graduating from Stanford in 2009, she worked at Google in the Online Partnerships Group, was a technical consultant where she onboarded new DoubleClick clients, and acted as interim Product Manager for internal insights tools.",
+        podcast : "The Untold Stories of Open Source",
+        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
+        featured : true
+    },
+    {
+        title : "An Introduction to the new Podcast from the Linux Foundation",
+        src : "https://open.spotify.com/embed/episode/62DAkdeQTSAPeLbxF2sTlX?utm_source=generator",
+        type : "spotify",
+        description : "Open Source is embedded in every software application you touch today. It's impossible to build a large scale application without it. The real question is, what's the story behind that component, application, or framework you just downloaded? Not the specs. Not the functionality. The real story: 'Who wrote the code? What is their backstory? What led them to the Open Source community?'",
+        podcast : "FINOS Open Source in Finance Podcast",
+        podcastUrl : "https://untold-stories-of-open-source.captivate.fm/spotify",
+        featured : true
+    }  
 ];
